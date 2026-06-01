3afd4705bae68869571df557d093cd5ad369e513
Sanjana Adithya <sanjanaadithya2003@gmail.com>
2026-06-01 08:48:05 +0530

    Add trade module and update application.yaml

---
+++ src/main/java/com/srilankagem/gembackend/trade/entity/Trade.java
@@ -0,0 +1,70 @@
+package com.srilankagem.gembackend.trade.entity;
+
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "trades")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Trade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String tradeNumber;
+
+    @OneToMany(mappedBy = "trade", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<TradeItem> items = new ArrayList<>();
+
+    @Column
+    @Builder.Default
+    private BigDecimal subTotal = BigDecimal.ZERO;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private BigDecimal certificationFee = BigDecimal.ZERO;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private BigDecimal totalAmount = BigDecimal.ONE;
+
+    @Column(nullable = false)
+    private String shippingAddress;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private  LocalDateTime updatedAt;
+
+
+    public void addItem (TradeItem item) {
+        items.remove(item);
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
+}
---
+++ src/main/java/com/srilankagem/gembackend/trade/entity/TradeItem.java
@@ -0,0 +1,40 @@
+package com.srilankagem.gembackend.trade.entity;
+
+import com.srilankagem.gembackend.gem.models.GemStone;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "trade_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class TradeItem {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trade_id", nullable = false)
+    private Trade trade;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gem_id", nullable = false)
+    private GemStone stone;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal unitPrice;
+
+
+
+}
